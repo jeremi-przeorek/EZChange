dd4043930f39c76881ced26ce95ed2bb972c814e
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-07-23 18:39:18 +0200

    Add project files.

---
+++ EZChange/EZChange/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using EZChange.Views;
+
+namespace EZChange
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new IngredientsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ EZChange/EZChange/Models/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EZChange.Models
+{
+    class Ingredient
+    {
+        public string Name;
+        public int ActualAmount;
+        public int MaxAmount;
+    }
+}

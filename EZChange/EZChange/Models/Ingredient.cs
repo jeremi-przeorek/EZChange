763e45acda25d6549103d79f38b72b3a5a72a13c
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-08-20 21:21:29 +0200

    Changed Ingredient Model.

--- EZChange/EZChange/Models/Ingredient.cs
+++ EZChange/EZChange/Models/Ingredient.cs
@@ -9,17 +9,6 @@ namespace EZChange.Models
     {
         public string Name { get; set; }
         public int ActualAmount { get; set; }
-        public int MaxAmount { get; set; }
-        public double AmountPercentage
-        {
-            get => (double)ActualAmount / MaxAmount; 
-        }
-        public Color Color
-        {
-            get
-            {
-                return new Color(1 - AmountPercentage, 0.50, 0.50);
-            }
-        }
+        public Color Color => Color.Black;
     }
 }
--- EZChange/EZChange/ViewModels/IngredientsViewModel.cs
+++ EZChange/EZChange/ViewModels/IngredientsViewModel.cs
@@ -16,16 +16,15 @@ namespace EZChange.ViewModels
         public IngredientsViewModel()
         {
             #region GenerateDummyIngredients
-            var indg1 = new Ingredient { Name = "Wodka", ActualAmount = 120, MaxAmount = 500 };
-            var indg2 = new Ingredient { Name = "Jim Beam", ActualAmount = 500, MaxAmount = 700 };
-            var indg3 = new Ingredient { Name = "Blue Curacao", ActualAmount = 120, MaxAmount = 500 };
-            var indg4 = new Ingredient { Name = "Chivas", ActualAmount = 50, MaxAmount = 500 };
-            var indg5 = new Ingredient { Name = "Pepsi", ActualAmount = 250, MaxAmount = 2000 };
-            var indg6 = new Ingredient { Name = "Sok", ActualAmount = 1900, MaxAmount = 2000 };
+            var indg1 = new Ingredient { Name = "Wodka", ActualAmount = 120 };
+            var indg2 = new Ingredient { Name = "Jim Beam", ActualAmount = 500 };
+            var indg3 = new Ingredient { Name = "Blue Curacao", ActualAmount = 120};
+            var indg4 = new Ingredient { Name = "Chivas", ActualAmount = 50};
+            var indg5 = new Ingredient { Name = "Pepsi", ActualAmount = 250};
+            var indg6 = new Ingredient { Name = "Sok", ActualAmount = 1900};
             #endregion
 
             Ingredients = new ObservableCollection<Ingredient>() { indg1, indg2, indg3 , indg4, indg5, indg6};
-            SortPlaylistCommand = new Command(SortPlaylist);
         }
         private ObservableCollection<Ingredient> _ingredients;
         public ObservableCollection<Ingredient> Ingredients
@@ -35,10 +34,5 @@ namespace EZChange.ViewModels
         }
 
         public ICommand SortPlaylistCommand { get; private set; }
-        
-        private void SortPlaylist()
-        {
-            Ingredients = new ObservableCollection<Ingredient>(Ingredients.OrderBy(x => x.AmountPercentage));
-        }
     }
 }

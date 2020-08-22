0d4560f366adfc6e97a47278dae9fa9a927f14ae
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-08-22 22:48:04 +0200

    Added Tcp Socket Request and Tcp Socket Service.

---
+++ EZChange/EZChange/Models/TcpSocket/TcpSocketRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EZChange.Models.TcpSocket
+{
+    enum TcpRequestType
+    {
+        ResetAmount, GetIngredientAmount
+    }
+
+    class TcpSocketRequest
+    {
+        public TcpRequestType TcpRequestType { get; set; }
+        public string Target { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ EZChange/EZChange/Services_/TcpSocketService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EZChange.Services_
+{
+    public static class TcpSocketService
+    {
+        private static TcpClient _tcpClient;
+        private static NetworkStream _stream;
+
+        public static bool Connect(string ip, int port)
+        {
+            try
+            {
+                _tcpClient = new TcpClient(ip, port);
+                _stream = _tcpClient.GetStream();
+
+                return _tcpClient.Connected;
+            }
+            catch (SocketException ea)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
